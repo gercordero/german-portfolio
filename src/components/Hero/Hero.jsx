67c83f053204ed1bf9f77c776ebdb1d153581d5d
German Cordero <gerrcordero@gmail.com>
2020-10-23 12:39:54 +0200

    Hero section reworked for better responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import heroSvg from "../../assets/hero-svg.svg"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../../constants/socialLinks"
-import { Container, Col, Row } from "react-bootstrap"
+import { Container } from "react-bootstrap"
 import "./styles/hero.scss"
 
 const heroImageQuery = graphql`
@@ -30,23 +30,21 @@ const Hero = () => {
   return (
     <header className="hero">
       <div className="hero-grid">
-        <Container className="hero-center">
-          <Row>
-            <Col className="hero-info">
-              <h1 className="display-4">I’m Germán</h1>
-              <h2>Front-end Web Developer & Designer</h2>
-              <Link to="/contact/" className="button hero-button">
-                contact me
-              </Link>
-              <SocialLinks />
-            </Col>
-            <Col className="hero-image">
-              <Image fluid={fluid} alt="Hero image" />
-            </Col>
-          </Row>
-        </Container>
-        <div className="hero-image-footer">
-          <img src={heroSvg} alt="Hero Desktop Svg" />
+        <div className="hero-center">
+          <div className="hero-info">
+            <h1 className="display-4">I’m Germán</h1>
+            <h2>Front-end Web Developer & Designer</h2>
+            <Link to="/contact/" className="button hero-button">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+          <div className="hero-image">
+            <Image fluid={fluid} alt="Hero image" />
+          </div>
+          <div className="hero-image-footer">
+            <img src={heroSvg} alt="Hero Desktop Svg" />
+          </div>
         </div>
         <div className="hero-footer" />
       </div>
