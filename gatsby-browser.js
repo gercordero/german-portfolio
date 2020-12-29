1b419df42545f4a79b302340c4796fb226a2a8eb
German <gercordero111@gmail.com>
2020-12-29 23:54:45 +0100

    Added loading screen

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,23 @@
+// Page styles
 import "./src/styles/main.css"
+// Fonts
 import "fontsource-open-sans"
 import "fontsource-roboto"
+// Wrap every component inside Layout component
+import CustomLayout from "./src/gatsby/browser/wrapPageElement"
+import { disableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock"
 
 // Apply layout to every page
-import CustomLayout from "./src/gatsby/browser/wrapPageElement"
 export const wrapPageElement = CustomLayout
+
+// Hide loading svg overlay when page is ready
+export const onInitialClientRender = () => {
+  // Block scroll until page is ready
+  const body = document.getElementById("___gatsby")
+  disableBodyScroll(body)
+
+  setTimeout(function () {
+    document.getElementById("___loader").style.display = "none"
+    clearAllBodyScrollLocks()
+  }, 500)
+}
